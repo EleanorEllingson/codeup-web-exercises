2684dc33ccab32410fb89665754b8e9d6db32748
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-09-28 10:46:40 -0500

    Monday 9/2/ warmup

---
+++ js/warmup.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("Hello Joshua");
+console.log("Hello Ethan");
+console.log("Hello Rebecca");
+console.log("Hello Elena");
+console.log("Hello Michael");
+
+function sayHello(name) {
+    console.log("Hello " + name)
+}
+sayHello("Joshua")
+sayHello("Ethan")
+
