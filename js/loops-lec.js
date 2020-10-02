c65987bd6ed7f67f3d067e575bcf8f1481c08f32
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-10-02 14:36:20 -0500

    finished loops lecture

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -111,17 +111,23 @@ alert("Blue is my favorite too!")
 // }
 
 //TODO TOGETHER: Explore basic for loop syntax
-
+//for (var i = 0; i < 10; i++) {
+ //   console.log("for loop iteration #" + i);
+//}
 
 // TODO TOGETHER: Write a for loop that loops through numbers 0 - 5 (inclusive);
-
+for(var i = 0; i <= 5; i++){
+    console.log(i)
+}
 
 // TODO TOGETHER: Why are loops beneficial?
 
 
 // TODO TOGETHER: Console.log each color from the array randomColors
-//  var randomColors = ["red","orange","yellow","blue","green"]
-
+var randomColors = ["red","orange","yellow","blue","green"]
+for(var i = 0; i < randomColors.length; i++){
+    console.log(randomColors[i]);
+}
 
 // for (var i = 0; i < randomColors.length; i++){
 // 	console.log(randomColors[i]);;
@@ -130,9 +136,18 @@ alert("Blue is my favorite too!")
 
 
 // TODO TOGETHER: Using a for loop, log all even numbers 0 - 100;
-
+for(var i = 0; i <= 100; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
 
 // TODO: Using a for loop, log all numbers divisible by 5, from 0 - 100;
+for(var i = 0; i <= 100; i++){
+    if(i % 5 === 0){
+        console.log(i);
+    }
+}
 
 
 // TODO: Using a for loop, complete the FizzBuzz challenge:
@@ -141,6 +156,18 @@ alert("Blue is my favorite too!")
 //  For numbers divisible by 5, log "Buzz"
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
+for(var i = 0; i <= 100; i++){
+    if(i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if(i % 3 === 0) {
+        console.log("Fizz");
+    } else if(i % 5 === 0){
+            console.log("Buzz")
+    } else{
+            console.log(i);
+    }
+}
+
 
 
 
