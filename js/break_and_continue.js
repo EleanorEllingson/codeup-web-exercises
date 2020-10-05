fcb2a25c4a7402b6ac71ec363da5d11d0e15c672
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-10-05 13:35:12 -0500

    more fixes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,23 @@
 "use strict";
 
-var oddNumber = (prompt("Choose an odd number between 1 and 50"));
-for (var i = 1; i <= 50; i++){
-    if(oddNumber % 2 === 0){
-    prompt("Please enter a correct number");
-    break;
-    }
-}
 
-for (var i = 1; i <= 50; i = i + 2) {
+do {
+    var oddNumber = (prompt("Choose an odd number between 1 and 50"));
+
+    if(oddNumber < 1 || oddNumber > 50) {
+        prompt("Number is invalid, please enter a number between 1 and 50")
+        break;
+    }
+    else if (oddNumber % 2 === 0) {
+            prompt("Number is even, Please enter an odd number");
+            break;
+        } else {
+            alert("Number is valid! Thank you!")
+            break
+        }
+    } while (  );
+
+for (var i = 1; i <= 50; i+ 2) {
     if(i == oddNumber) {
         console.log("Yikes, skipping number: " + oddNumber)
         continue;
--- js/for_loops.js
+++ js/for_loops.js
@@ -5,14 +5,14 @@ function showMultiplicationTable(n){
 
     for(var i = 1; i <= 10; i++){
 
-        console.log((n + " X " + i + " = ") + (7 * i));
+        console.log((n + " X " + i + " = ") + (n * i));
     }
 } showMultiplicationTable(7)
 
 var randomNumber;
 //  console.log(randomNumber)
 for(var i = 1; i <= 10; i++){
-    randomNumber = Math.floor((Math.random() * (200 - 20 + 1)) + 20);
+    randomNumber = Math.floor((Math.random() * 180) + 20);
     if(randomNumber % 2 === 0){
         console.log(randomNumber + " is even");
     } else{
@@ -24,6 +24,6 @@ for(var i = 1; i <= 9; i++) {
     console.log(i.toString().repeat(i));
 }
 
-for(var i = 100; i >= 5; i = i - 5){
+for(var i = 100; i >= 5; i-=5){
     console.log(i);
 }
