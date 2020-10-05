0043d46dfc3a3916e00959013e4c6cd0da067882
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-10-05 10:01:45 -0500

    finished loops exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var oddNumber = (prompt("Choose an odd number between 1 and 50"));
+for (var i = 1; i <= 50; i++){
+    if(oddNumber % 2 === 0){
+    prompt("Please enter a correct number");
+    break;
+    }
+}
+
+for (var i = 1; i <= 50; i = i + 2) {
+    if(i == oddNumber) {
+        console.log("Yikes, skipping number: " + oddNumber)
+        continue;
+        {
+
+        }
+    }
+    console.log("Here is an odd number: " + i);
+
+
+}
---
+++ js/for_loops.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+function showMultiplicationTable(n){
+
+    for(var i = 1; i <= 10; i++){
+
+        console.log((n + " X " + i + " = ") + (7 * i));
+    }
+} showMultiplicationTable(7)
+
+var randomNumber;
+//  console.log(randomNumber)
+for(var i = 1; i <= 10; i++){
+    randomNumber = Math.floor((Math.random() * (200 - 20 + 1)) + 20);
+    if(randomNumber % 2 === 0){
+        console.log(randomNumber + " is even");
+    } else{
+        console.log(randomNumber + " is odd");
+    }
+}
+
+for(var i = 1; i <= 9; i++) {
+    console.log(i.toString().repeat(i));
+}
+
+for(var i = 100; i >= 5; i = i - 5){
+    console.log(i);
+}
