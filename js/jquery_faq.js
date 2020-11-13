fd149403000cf172a82c9d6a0e35b65e1f0df5c1
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-11-13 15:57:47 -0600

    traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,35 @@
-$('button').click(function (e) {
-    $('dd').toggleClass(function(){
-
+$('#definitions').click(function () {
+    $('dd').toggle();
 
 });
+
+
+
+
+$('#park-button').click(function () {
+
+    $('ul').each(function (i, e) {
+        $(e).children().last().css('background-color', 'yellow')
+
+    });
+    });
+
+$('h3').click(function () {
+
+    $('ul').each(function (i, e) {
+        $(e).children().css('font-weight', 'bold')
+
+    });
+});
+// $('h3').click(function (){
+//     $(this).parent().children('li').addClass('bold');
+//     $(this).next('ul').slideToggle(500)
+// })
+
+$('li').click(function () {
+
+    $('ul').each(function (i, e) {
+        $(e).children().first().css('color', 'blue')
+
+    });
 });
