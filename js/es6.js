203cfc2d28d53d8afd7c325d62c0b522a0c80ea2
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-11-19 17:03:56 -0600

    es6 exercise

--- js/es6.js
+++ js/es6.js
@@ -33,9 +33,15 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'eleanor';
+const email = 'eleanor.ellingson@gmail.com';
+const languages = ['html', 'css', 'javascript'];
+
+const {name, email, languages} = users;
+console.log(name);
+console.log(email);
+console.log(languages);
+
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
@@ -44,20 +50,26 @@ users.push({
   languages: languages
 });
 
+const users.push
+
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
 users.forEach(function(user) {
   return emails.push(user.email);
 });
+users.forEach((element) => {
+  console.log(`user.email`)
+})
+
 users.forEach(function(user) {
   return names.push(user.name);
 });
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
