ff16e16527712eb61cc578cc8cf2fcf8ed34712f
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-11-23 15:42:00 -0600

    started fetch

--- js/promises.js
+++ js/promises.js
@@ -1,17 +1,23 @@
 "use strict";
 
-function wait(numberMilliseconds) {
-    return new Promise((resolve, reject) => {
-
-        setTimeout(() => {
-
-            resolve();
-        }, numberMilliseconds);
-
-    });
-}
 
 
+fetch('https://api.github.com/events', {headers: {'Authorization': 'token ' + 'gitHubToken'}})
+    .then(response => console.log(response))
+    .catch(error => console.error(error))
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+// function wait(numberMilliseconds) {
+//     return new Promise((resolve, reject) => {
+//
+//         setTimeout(() => {
+//
+//             resolve();
+//         }, numberMilliseconds);
+//
+//     });
+// }
+//
+//
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
